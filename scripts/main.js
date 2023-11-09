a09d219b2310e38b8a4c20b0de61e47469f70501
Martins.py <74680291+Mart1ns@users.noreply.github.com>
2023-11-09 08:23:27 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -43,70 +43,68 @@ class Tarefa {
   
     // Exibe as tarefas na interface
     exibirTarefas() {
+        const tarefasLista = document.getElementById('tarefasLista');
+        tarefasLista.innerHTML = '';
     
-            const tarefasLista = document.getElementById('tarefasLista');
-            tarefasLista.innerHTML = '';
-        
-            this.lista.forEach((value, index) => {
-              const divTarefa = document.createElement('div');
-              divTarefa.className = 'tarefa-item';
-        
-              const pNome = document.createElement('p');
-              pNome.textContent = value.nome;
-              divTarefa.appendChild(pNome);
-        
-              const tooltip = document.createElement('div');
-              tooltip.className = 'tooltip';
-              tooltip.textContent = `Descrição: ${value.descricao}, Prioridade: ${value.prioridade}`;
-        
-              divTarefa.appendChild(tooltip);
-        
-              const buttonConcluir = document.createElement('button');
-              buttonConcluir.textContent = value.status ? 'Desconcluir' : 'Concluir';
-              buttonConcluir.id = 'botaoTarefa';
-              buttonConcluir.style.display = 'none';
-              buttonConcluir.addEventListener('click', () => {
-                value.status = !value.status;
-                divTarefa.style.textDecoration = value.status ? 'line-through' : 'none';
-                buttonConcluir.textContent = value.status ? 'Desconcluir' : 'Concluir';
-                this.salvarNoLocalStorage();
-              });
-              divTarefa.appendChild(buttonConcluir);
-        
-              const buttonExcluir = document.createElement('button');
-              buttonExcluir.textContent = 'Excluir';
-              buttonExcluir.id = 'botaoTarefa';
-              buttonExcluir.style.display = 'none';
-              buttonExcluir.addEventListener('click', () => {
-                this.lista.splice(index, 1);
-                this.contador -= 1;
-                this.exibirTarefas();
-                this.salvarNoLocalStorage();
-              });
-              divTarefa.appendChild(buttonExcluir);
-        
-              divTarefa.addEventListener('mouseover', () => {
-                tooltip.style.display = 'block';
-                buttonConcluir.style.display = 'block';
-                buttonExcluir.style.display = 'block';
-              });
-        
-              divTarefa.addEventListener('mouseout', () => {
-                tooltip.style.display = 'none';
-                buttonConcluir.style.display = 'none';
-                buttonExcluir.style.display = 'none';
-              });
-        
-              tarefasLista.appendChild(divTarefa);
-            });
-        
-            const contador = document.createElement('p');
-            contador.textContent = `Total de Tarefas: ${this.contador}`;
-            contador.className = 'contador-tarefas';
-        
-            tarefasLista.appendChild(contador);
-          }
-    }
+        this.lista.forEach((value, index) => {
+          const divTarefa = document.createElement('div');
+          divTarefa.className = 'tarefa-item';
+    
+          const pNome = document.createElement('p');
+          pNome.textContent = value.nome;
+          divTarefa.appendChild(pNome);
+    
+          const tooltip = document.createElement('div');
+          tooltip.className = 'tooltip';
+          tooltip.textContent = `Descrição: ${value.descricao}, Prioridade: ${value.prioridade}`;
+    
+          divTarefa.appendChild(tooltip);
+    
+          const buttonConcluir = document.createElement('button');
+          buttonConcluir.textContent = value.status ? 'Desconcluir' : 'Concluir';
+          buttonConcluir.id = 'botaoTarefa';
+          buttonConcluir.style.display = 'none';
+          buttonConcluir.addEventListener('click', () => {
+            value.status = !value.status;
+            divTarefa.style.textDecoration = value.status ? 'line-through' : 'none';
+            buttonConcluir.textContent = value.status ? 'Desconcluir' : 'Concluir';
+            this.salvarNoLocalStorage();
+          });
+          divTarefa.appendChild(buttonConcluir);
+    
+          const buttonExcluir = document.createElement('button');
+          buttonExcluir.textContent = 'Excluir';
+          buttonExcluir.id = 'botaoTarefa';
+          buttonExcluir.style.display = 'none';
+          buttonExcluir.addEventListener('click', () => {
+            this.lista.splice(index, 1);
+            this.contador -= 1;
+            this.exibirTarefas();
+            this.salvarNoLocalStorage();
+          });
+          divTarefa.appendChild(buttonExcluir);
+    
+          divTarefa.addEventListener('mouseover', () => {
+            tooltip.style.display = 'block';
+            buttonConcluir.style.display = 'block';
+            buttonExcluir.style.display = 'block';
+          });
+    
+          divTarefa.addEventListener('mouseout', () => {
+            tooltip.style.display = 'none';
+            buttonConcluir.style.display = 'none';
+            buttonExcluir.style.display = 'none';
+          });
+    
+          tarefasLista.appendChild(divTarefa);
+        });
+    
+        const contador = document.createElement('p');
+        contador.textContent = `Total de Tarefas: ${this.contador}`;
+        contador.className = 'contador-tarefas';
+    
+        tarefasLista.appendChild(contador);
+      }
   
     // Limpa os campos do modal após adicionar uma tarefa
     limparCampos() {
@@ -114,7 +112,7 @@ class Tarefa {
       document.getElementById('descricaoTarefaModal').value = '';
       document.getElementById('prioridadeModal').value = 'baixa';
     }
-  
+  }
   
   // Instância da classe Lista para gerenciar as tarefas
   const tarefas = new Lista();
